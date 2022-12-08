e34f75a1c59aec86eeea48b8ce523e184d3d8940
snukalov <snukalov@gmail.com>
2022-12-08 16:11:52 +0200

    Fixed toolchain for cross-compile

--- cmake/aarch64-rpi4-linux-gnu.cmake
+++ cmake/aarch64-rpi4-linux-gnu.cmake
@@ -4,27 +4,27 @@ include(CMakeForceCompiler)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
+set(TOOLCHAIN_PREFIX aarch64-linux-gnu-)
+
 # which compilers to use for C and C++
-cmake_force_c_compiler(/usr/bin/aarch64-linux-gnu-gcc GNU)
-cmake_force_cxx_compiler(/usr/bin/aarch64-linux-gnu-g++ GNU)
-# set(CMAKE_C_COMPILER=/usr/bin/aarch64-linux-gnu-gcc GNU)
-# set(CMAKE_CXX_COMPILER=/usr/bin/aarch64-linux-gnu-g++ GNU)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 
 set(CMAKE_SYSROOT  $ENV{RASPBIAN_ROOTFS})
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT}) 
 set(CMAKE_LIBRARY_ARCHITECTURE aarch64-linux-gnu)
 
-set(CMAKE_LIBRARY_PATH=$ENV{RASPBIAN_ROOTFS}/usr/local/lib)
-set(CMAKE_INSTALL_PREFIX=$ENV{RASPBIAN_ROOTFS}/usr/local)
-set(CMAKE_AR=/usr/bin/aarch64-linux-gnu-ar)
-set(CMAKE_C_COMPILER_AR=/usr/bin/aarch64-linux-gnu-gcc-ar)
-set(CMAKE_C_COMPILER_RANLIB=/usr/bin/aarch64-linux-gnu-gcc-ranlib)
-set(CMAKE_LINKER=/usr/bin/aarch64-linux-gnu-ld)
-set(CMAKE_NM=/usr/bin/aarch64-linux-gnu-nm)
-set(CMAKE_OBJCOPY=/usr/bin/aarch64-linux-gnu-objcopy)
-set(CMAKE_OBJDUMP=/usr/bin/aarch64-linux-gnu-objdump)
-set(CMAKE_RANLIB=/usr/bin/aarch64-linux-gnu-ranlib)
-set(CMAKE_STRIP=/usr/bin/aarch64-linux-gnu-strip)
+set(CMAKE_LIBRARY_PATH $ENV{RASPBIAN_ROOTFS}/usr/local/lib)
+set(CMAKE_INSTALL_PREFIX $ENV{RASPBIAN_ROOTFS}/usr/local)
+set(CMAKE_AR ${TOOLCHAIN_PREFIX}ar)
+set(CMAKE_C_COMPILER_AR ${TOOLCHAIN_PREFIX}gcc-ar)
+set(CMAKE_C_COMPILER_RANLIB ${TOOLCHAIN_PREFIX}gcc-ranlib)
+set(CMAKE_LINKER ${TOOLCHAIN_PREFIX}ld)
+set(CMAKE_NM ${TOOLCHAIN_PREFIX}nm)
+set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
+set(CMAKE_RANLIB ${TOOLCHAIN_PREFIX}ranlib)
+set(CMAKE_STRIP ${TOOLCHAIN_PREFIX}strip)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
@@ -34,3 +34,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE arm64)
